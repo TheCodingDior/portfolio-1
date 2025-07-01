6ff0ef23799b8951c24eb519ef9d65c234c7c995
Ayan Ghosh <151054954+TheCodingDior@users.noreply.github.com>
2025-07-01 20:32:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ ScrollReveal({
     delay:200
 });
 
-ScrollReveal().reveal('.home-content,.heading',{origin:'top'});
+
 ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,.contact form',{origin:'bottom'});
 ScrollReveal().reveal('.home-content h1',{origin:'left'});
 ScrollReveal().reveal('.home-content p',{origin:'right'});
